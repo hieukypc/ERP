1c8fead4760c04a3fa9ca4dca8a6bcd4131e4e2f
HieukyPC <hieuky.pc@gmail.com>
2016-09-04 23:27:01 +0700

    Add source code ERP to GitHub

---
+++ openerp/addons/daily_transaction/__openerp__.py
@@ -0,0 +1,15 @@
+{
+	'name': 'daily_transaction',
+	'version': '1.0',
+	'category': 'manager',
+	'description': """
+The module will  manage our daily transactions where we'll keep a log of where and what money we have spent on a particular day.
+======================================================
+	""",
+	'author': 'PC',
+	'depends': [],
+	'data': ['daily_transaction_view.xml'],
+	'demo': [],
+	'installable': True,
+	'auto_install': False,
+}
---
+++ openerp/addons/daily_transaction/daily_transaction.py
@@ -0,0 +1,18 @@
+from openerp.osv import fields, osv
+
+class daily_transation(osv.osv):
+    _name = "daily.transaction"
+    _description = "Daily Transaction"
+    
+    _columns = {
+        'subject': fields.char('Subject', size=128, require=True),
+        'date': fields.date('Date', require=True),
+        'note': fields.text('Notes'),
+        'amount': fields.float('Amount', require=True, digits=lambda cr: (32, 32)),
+        'type': fields.selection([
+            ('transport', 'Transport'),
+            ('household', 'Household'),
+            ('personal', 'Personal'),
+            ], 'Type', requrie=True),
+               
+    }
